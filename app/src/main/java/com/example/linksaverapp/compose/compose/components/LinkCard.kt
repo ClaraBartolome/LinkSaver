b15549dd832a9d5efc51c2ca7610b5c1d4ef0e79
Clara Bartolome Pereira <patatita300@gmail.com>
2023-11-15 10:28:41 +0100

    Added new button
    Added new button in linkcard to open bottomsheet

--- app/src/main/java/com/example/linksaverapp/compose/compose/components/LinkCard.kt
+++ app/src/main/java/com/example/linksaverapp/compose/compose/components/LinkCard.kt
@@ -1,14 +1,18 @@
 package com.example.linksaverapp.compose.compose.components
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
@@ -16,11 +20,13 @@ import androidx.compose.material.Text
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.linksaverapp.R
 import com.example.linksaverapp.ui.theme.LinkSaverAppTheme
 import com.example.linksaverapp.ui.theme.lightGreen
 
@@ -37,31 +43,45 @@ fun LinkCard(
             .padding(horizontal = 16.dp, vertical = 8.dp)
             .fillMaxSize()
             .combinedClickable(
-                onClick = onClickLink,
-                onLongClick = onLinkLongPressed
+                //AQUI EL SELECTABLE
+                onClick = {},
+                onLongClick = {}
             ),
         shape = RoundedCornerShape(10.dp),
         colors = CardDefaults.cardColors(
             containerColor = lightGreen,
         )
     ) {
-        Column {
-            Text(
-                text = title,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.padding( top = 8.dp, start = 16.dp, end = 16.dp, bottom = 4.dp)
-            )
-            Text(
-                text = link,
-                maxLines = 1,
-                style = MaterialTheme.typography.subtitle2,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.padding(horizontal = 16.dp)
-            )
-            Spacer(modifier = Modifier.height(8.dp))
+        Row(modifier = Modifier
+            .wrapContentHeight()
+            .fillMaxWidth()
+            .padding(start = 16.dp, top = 8.dp, bottom = 8.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically){
+            Column(modifier = Modifier
+                .weight(1f)
+                .combinedClickable(
+                    onClick = onClickLink,
+                    onLongClick = onLinkLongPressed
+                )) {
+                Text(
+                    text = title,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.padding(bottom = 4.dp)
+                )
+                Text(
+                    text = link,
+                    maxLines = 1,
+                    style = MaterialTheme.typography.subtitle2,
+                    overflow = TextOverflow.Ellipsis,
+                )
+            }
+            Column(modifier = Modifier
+                .fillMaxHeight()
+                .clickable { onLinkLongPressed.invoke() }){
+                Image(painter = painterResource(id = R.drawable.ic_more), contentDescription = "", modifier = Modifier
+                    .size(32.dp))
+            }
         }
-
     }
 }
 
